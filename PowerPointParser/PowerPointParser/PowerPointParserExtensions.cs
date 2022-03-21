a1585bcc763b317e9f45d12eec6bb62df999a1f1
aloksh <aloksh@webmd.net>
2022-03-21 15:21:26 -0400

    Refactor code

---
+++ PowerPointParser/PowerPointParser/PowerPointParserExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Aaks.PowerPointParser.Dto;
+
+namespace Aaks.PowerPointParser;
+
+public static class PowerPointParserExtensions
+{
+    public static Queue<OpenXmlParagraphWrapper> ToQueue(this IDictionary<int, IList<OpenXmlParagraphWrapper>> items)
+    {
+        Queue<OpenXmlParagraphWrapper> openXmlParagraphWrappers = new();
+        var xmlParagraphWrappers = items.Select(x => x.Value).SelectMany(y => y).ToList();
+        foreach (var openXmlParagraphWrapper in xmlParagraphWrappers)
+        {
+            openXmlParagraphWrappers.Enqueue(openXmlParagraphWrapper!);
+        }
+        return openXmlParagraphWrappers;
+    }
+}
